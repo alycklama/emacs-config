f70d987318650a55d843bd0e4d124b4fef484f2e
Anton Lijcklama à Nijeholt <anton.lijcklama.a.nijeholt@ing.nl>
2017-06-20 13:59:35 +0200

    Added select all shortcut

--- init.el
+++ init.el
@@ -72,6 +72,7 @@
 (global-set-key (kbd "s-c") 'kill-ring-save)
 (global-set-key (kbd "s-v") 'yank)
 (global-set-key (kbd "s-x") 'kill-region)
+(global-set-key (kbd "s-a") 'mark-whole-buffer)
 (global-set-key (kbd "M-<down>") 'move-text-line-down)
 (global-set-key (kbd "M-<up>") 'move-text-line-up)
 (global-set-key (kbd "s-/") 'comment-region)
