ef8bfab1ac8ac1057d5f88e95dc9c3b576f9cb73
Anton Lycklama à Nijeholt <alycklama@Antons-MacBook-Pro-2.local>
2017-06-21 19:55:41 +0200

    Installed column-enforce-mode

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(package-selected-packages
    (quote
-    (org evil-anzu exec-path-from-shell spaceline emmet-mode htmlize move-text linum-relative gitignore-mode dired-narrow sunrise-commander web-mode sublime-themes git-timemachine restclient haskell-mode protobuf-mode ranger paradox org-babel-eval-in-repl ox-gfm smart-mode-line autopair dtrt-indent evil-magit column-marker wc-mode evil-mc evil-commentary json-mode use-package dracula-theme badger-theme reykjavik-theme rainbow-mode highlight-thing ivy-bibtex org-ref writeroom-mode expand-region evil ivy ample-theme auto-complete magit ensime counsel-projectile rainbow-delimiters)))
+    (column-enforce-mode org evil-anzu exec-path-from-shell spaceline emmet-mode htmlize move-text linum-relative gitignore-mode dired-narrow sunrise-commander web-mode sublime-themes git-timemachine restclient haskell-mode protobuf-mode ranger paradox org-babel-eval-in-repl ox-gfm smart-mode-line autopair dtrt-indent evil-magit column-marker wc-mode evil-mc evil-commentary json-mode use-package dracula-theme badger-theme reykjavik-theme rainbow-mode highlight-thing ivy-bibtex org-ref writeroom-mode expand-region evil ivy ample-theme auto-complete magit ensime counsel-projectile rainbow-delimiters)))
  '(paradox-github-token t)
  '(safe-local-variable-values (quote ((org-confirm-babel-evaluate))))
  '(sml/theme (quote automatic)))
@@ -149,13 +149,15 @@
 	(add-to-list 'exec-path "/usr/local/bin")
 	(setq ensime-search-interface 'ivy)
 
+  :config
+  (column-enforce-mode 1)
+
   :bind (:map ensime-mode-map
     ("s-<return>" . ensime-edit-definition)
     ("M-<return>" . ensime-import-type-at-point)
     ("C-<return>" . ensime-type-at-point)
     ("M-s-f" . ensime-search)
-  )
-)
+  ))
 
 ;; Counsel Projectile
 (require 'counsel-projectile)
